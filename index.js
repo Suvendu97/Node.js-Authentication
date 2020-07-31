1c6baab2f5c84dcb4483aad4c4933080c545c6d4
Suvendu97 <57838911+Suvendu97@users.noreply.github.com>
2020-07-31 22:59:29 +0530

    middleware, flash messages and NOTY added

--- index.js
+++ index.js
@@ -2,24 +2,35 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const app = express();
 const port = 8000;
-// const expressLayouts = require('express-ejs-layouts');
+const expressLayouts = require('express-ejs-layouts');
 const db = require('./config/mongoose');
 // used for session cookie
 const session = require('express-session');
 const passport = require('passport');
 const passportLocal = require('./config/passport-local-strategy');
 const MongoStore = require('connect-mongo')(session);
+const sassMiddleware = require('node-sass-middleware');
+const flash = require('connect-flash');
+const customMware = require('./config/middleware');
 
+
+app.use(sassMiddleware({
+    src: './assets/scss',
+    dest: './assets/css',
+    debug: true,
+    outputStyle: 'extended',
+    prefix: '/css'
+}));
 app.use(express.urlencoded());
 
 app.use(cookieParser());
 
-// app.use(express.static('./assets'));
+app.use(express.static('./assets'));
 
-// app.use(expressLayouts);
-// // extract style and scripts from sub pages into the layout
-// app.set('layout extractStyles', true);
-// app.set('layout extractScripts', true);
+app.use(expressLayouts);
+// extract style and scripts from sub pages into the layout
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
 
 
 
@@ -54,6 +65,9 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.use(passport.setAuthenticatedUser);
+
+app.use(flash());
+app.use(customMware.setFlash);
 
 // use express router
 app.use('/', require('./routes'));
